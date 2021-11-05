d0f79dab5bee156112f47fbb705eaa7cebe2436d
hayoungzzzzz <hayoung474@gmail.com>
2021-11-05 14:56:32 +0900

    사이드바 메뉴변경

--- src/Components/AsideMenuBar.js
+++ src/Components/AsideMenuBar.js
@@ -1,59 +1,95 @@
-import React, { useState } from "react";
+import React from "react";
 import styled, { css } from "styled-components";
-import {useHistory} from 'react-router-dom'
-import { RiUserSettingsFill, RiDashboardFill } from "react-icons/ri";
-import { MdViewHeadline, MdOutlineAdd, MdSettings } from "react-icons/md";
-
+import { useHistory } from "react-router-dom";
+import { RiUserSettingsFill, RiLogoutBoxRLine,RiDashboardFill } from "react-icons/ri";
+import {
+  MdOutlineCategory,
+  MdWebAsset,
+  MdCalendarViewDay,
+  MdOutlineAdd,
+  MdSettings,
+} from "react-icons/md";
+import { FaBook } from "react-icons/fa";
 function AsideMenuBar() {
   let history = useHistory();
-  const [subMenuToggle, setSubMenuToggle] = useState(false);
-  const _subMenuToggleHandler = () => {
-    setSubMenuToggle(!subMenuToggle);
-  };
 
-  const _goDashboard = ()=>{history.push("/dashborad")}
-  const _goUserManagement = ()=>{history.push("/userManagement")}
-  const _goSingleDataRegister = ()=>{history.push("/dataRegister/single")}
-  const _goExcelDataRegister = ()=>{history.push("/dataRegister/excel")}
-  const _goDocumentList = ()=>{history.push("/docs/list")}
-  const _goCrawlDataList = ()=>{history.push("/crawl/list/0")}
-  const _goCrawlCurationDataList = ()=>{history.push("/crawl/list/0")} // ... 라우터 이름을 뭘로하지 ... 
-  
+  const _goDashboard = () => {
+    history.push("/dashborad");
+  };
+  const _goUserManagement = () => {
+    history.push("/userManagement");
+  };
+  const _goSingleDataRegister = () => {
+    history.push("/dataRegister/single");
+  };
+  const _goExcelDataRegister = () => {
+    history.push("/dataRegister/excel");
+  };
+  const _goDocumentList = () => {
+    history.push("/docs/list");
+  };
+  const _goCrawlDataList = () => {
+    history.push("/crawl/list/0");
+  };
+  const _goCrawlCurationDataList = () => {
+    history.push("/crawl/list/0");
+  }; // ... 라우터 이름을 뭘로하지 ...
+
   return (
     <>
       <AsideMenuBarContainer>
         <AsideMenuBarWrapper>
           <AsideMenuBarItem>
             <RiDashboardFill className="icon" size="18" />
-            대쉬보드
+            대시보드
           </AsideMenuBarItem>
           <AsideMenuBarItem>
             <RiUserSettingsFill className="icon" size="18" />
             사용자 관리
           </AsideMenuBarItem>
-          <AsideMenuBarSubWrapper onClick={_subMenuToggleHandler}>
-            <p className="subWrapperTitle">
-              <MdOutlineAdd className="icon" size="18" />
-              데이터등록
-            </p>
-            <AsideMenuBarSubItem active={subMenuToggle}>
-              단일 데이터 등록
-            </AsideMenuBarSubItem>
-            <AsideMenuBarSubItem active={subMenuToggle}>
-              대량 데이터 등록
-            </AsideMenuBarSubItem>
-          </AsideMenuBarSubWrapper>
           <AsideMenuBarItem>
-            <MdViewHeadline className="icon" size="18" />
-            데이터 조회
+            <MdOutlineCategory className="icon" size="18" />
+            카테고리 관리
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <MdWebAsset className="icon" size="18" />
+            사이트 목록 관리
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <MdCalendarViewDay className="icon" size="18" />
+            크롤 데이터 조회
           </AsideMenuBarItem>
           <AsideMenuBarItem>
             <MdSettings className="icon" size="18" />
+            크롤 데이터 정제
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <MdOutlineAdd className="icon" size="18" />
             크롤 데이터 등록
           </AsideMenuBarItem>
           <AsideMenuBarItem>
-            <MdViewHeadline className="icon" size="18" />
-            크롤 데이터 조회
+            <MdCalendarViewDay className="icon" size="18" />
+            데이터 조회
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <MdOutlineAdd className="icon" size="18" />
+            단일 데이터 등록
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <MdOutlineAdd className="icon" size="18" />
+            대량 데이터 등록
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <FaBook className="icon" size="18" />
+            다국어 사전 관리
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <MdSettings className="icon" size="18" />
+            맟춤형 화면 생성
+          </AsideMenuBarItem>
+          <AsideMenuBarItem>
+            <RiLogoutBoxRLine className="icon" size="18" />
+            로그아웃
           </AsideMenuBarItem>
         </AsideMenuBarWrapper>
       </AsideMenuBarContainer>
@@ -61,17 +97,15 @@ function AsideMenuBar() {
   );
 }
 
-
 const AsideMenuBarContainer = styled.aside`
-  width:100%;
+  width: 100%;
 `;
 const AsideMenuBarWrapper = styled.ul`
   list-style-type: none;
   padding: 0;
   margin: 0;
   background-color: #eee;
-  height:100%;
-
+  height: 100%;
 `;
 const AsideMenuBarItem = styled.li`
   cursor: pointer;
