7bbfdbaee9ca766f341b351f439724c1cc61513d
hayoungzzzzz <hayoung474@gmail.com>
2021-11-22 20:25:05 +0900

    사용자 관리 메인 페이지 디자인 수정

--- src/Pages/Manager/UserManagement/UserManagement.js
+++ src/Pages/Manager/UserManagement/UserManagement.js
@@ -1,83 +1,130 @@
 import React from "react";
 import FormHeader from "../../../Components/FormHeader";
 import styled from "styled-components";
-
-
+import {
+  AiOutlineUserDelete,
+  AiOutlineCheck,
+  AiOutlineUserAdd,
+} from "react-icons/ai";
+import { RiUserSettingsLine } from "react-icons/ri";
 function UserManagement({
-    userData,
-    openUserModifyModal,
-    deleteUser,
-    openUserAddModal
-}){
-  
-
-    return(
+  userData,
+  openUserModifyModal,
+  deleteUser,
+  openUserAddModal,
+}) {
+  return (
     <>
-        <TableWrapper>
-          <CustomTable>
-            <colgroup>
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "30%" }} />
-              <col style={{ width: "15%" }} />
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "15%" }} />
-              <col style={{ width: "10%" }} />
-            </colgroup>
+      <FormHeader type={"view"} title={"사용자 관리"} />
+      <TableWrapper>
+        <CustomTable>
+          <colgroup>
+            <col style={{ width: "5%" }} />
+            <col style={{ width: "10%" }} />
+            <col style={{ width: "10%" }} />
+            <col style={{ width: "15%" }} />
+            <col style={{ width: "10%" }} />
+            <col style={{ width: "10%" }} />
+            <col style={{ width: "15%" }} />
+            <col style={{ width: "10%" }} />
+            <col style={{ width: "15%" }} />
+          </colgroup>
 
-            <thead>
-              <tr>
-                <th>선택</th>
-                <th>itemID</th>
-                <th>요약</th>
-                <th>발행자/발행기관</th>
-                <th>언어</th>
-                <th>수집일</th>
-                <th>페이지수</th>
-              </tr>
-            </thead>
-            <tbody>
-              {userData.map((user, index) => {
-                return (
-                  <tr key={index}>
-                    <td>
-                    </td>
-                    <td>{user.userID}</td>
-                    <td>{user.Name}</td>
-                    <td>{user.Email}</td>
-                    <td>{user.Company}</td>
-                    <td>{user.CreateAt}</td>
-                    <td>{user.Category}</td>
-                    <td>
-                      <Button onClick={()=>{openUserModifyModal(user)}}>수정</Button>
-                      <Button onClick={()=>{deleteUser(user.id)}}>삭제</Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </CustomTable>
-        </TableWrapper>
-        <Button onClick={()=>{openUserAddModal()}}>추가</Button>
+          <thead>
+            <tr>
+              <th>UID</th>
+              <th>사용자 ID</th>
+              <th>사용자 이름</th>
+              <th>이메일</th>
+              <th>연락처</th>
+              <th>소속</th>
+              <th>계정 생성일</th>
+              <th>사용자 권한</th>
+              <th>관리</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{user.id}</td>
+                  <td>{user.userID}</td>
+                  <td>{user.Name}</td>
+                  <td>{user.Email}</td>
+                  <td>{user.Tel}</td>
+                  <td>{user.Company}</td>
+                  <td>{user.CreateAt.substring(0, 19).replace("T", " ")}</td>
+                  <td>{user.Category}</td>
+                  <td>
+                    <ButtonWrapper>
+                      <Button
+                        onClick={() => {
+                          openUserModifyModal(user);
+                        }}
+                        color="#6DAF44"
+                      >
+                        <RiUserSettingsLine size="22" color="white" />
+                        <p>수정</p>
+                      </Button>
+                      <Button
+                        onClick={() => {
+                          deleteUser(user.id);
+                        }}
+                        color="#dc3545"
+                      >
+                        <AiOutlineUserDelete size="22" color="white" />
+                        <p>삭제</p>
+                      </Button>
+                    </ButtonWrapper>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </CustomTable>
+      </TableWrapper>
+      <Button
+        color="#435269"
+        onClick={() => {
+          openUserAddModal();
+        }}
+      >
+        <AiOutlineUserAdd size="22" color="white" />
+        <p>사용자 추가</p>
+      </Button>
     </>
-    );
+  );
 }
 
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
 const Button = styled.button`
+  margin: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.3rem 1rem 0.3rem 1rem;
+  border-radius: 5px;
+  border: none;
   background-color: ${(props) => props.color || "grey"};
   cursor: pointer;
   min-width: 5rem;
-  border: none;
-  border-radius: 4px;
-  color: white;
-  font-weight: bold;
-  margin: 0 0.5rem 0 0.5rem;
+  p {
+    margin-left: 0.5rem;
+    font-size: 12px;
+    font-weight: bold;
+    color: white;
+  }
 `;
 const TableWrapper = styled.div`
   width: 100%;
   max-height: 65rem;
   overflow: auto;
-  box-shadow : rgb(9 30 66 / 25%) 0px 1px 1px;
+  box-shadow: rgb(9 30 66 / 25%) 0px 1px 1px;
 `;
 const CustomTable = styled.table`
   width: 100%;
@@ -100,6 +147,5 @@ const CustomTable = styled.table`
     height: 20px; /*Desired height*/
   }
 `;
-
 
 export default UserManagement;
