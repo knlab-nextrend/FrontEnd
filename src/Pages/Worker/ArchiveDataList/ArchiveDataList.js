91252e67af60419ba8cbd6f46b509ac56c646ead
hayoungzzzzz <hayoung474@gmail.com>
2021-11-23 20:11:04 +0900

    사용자 관리 ui 수정

--- src/Pages/Worker/ArchiveDataList/ArchiveDataList.js
+++ src/Pages/Worker/ArchiveDataList/ArchiveDataList.js
@@ -1,9 +1,17 @@
 import React from "react";
-import FormHeader from '../../../Components/FormHeader'
-function ArchiveDataList() {
+import FormHeader from "../../../Components/FormHeader";
+function ArchiveDataList({ archiveDataList }) {
   return (
     <>
       <FormHeader type="view" title={"아카이브 데이터 조회"} />
+      {archiveDataList.map((item, index) => {
+        return (
+          <div>
+            <p>{item.item_id}</p>
+            <p>{item.page}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
--- src/Pages/Worker/ArchiveDataList/ArchiveDataListContainer.js
+++ src/Pages/Worker/ArchiveDataList/ArchiveDataListContainer.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArchiveDataList from "./ArchiveDataList";
+import { CrawlDataListFetchApi } from "../../../Utils/api";
+
 function ArchiveDataListContainer() {
+  /* 현재 보여질 데이터 */
+  const [archiveDataList, setArchiveDataList] = useState([]);
+
+  /* 페이지네이션 */
+  const [dcCount, setDcCount] = useState(0); // document 총 개수
+  const [pageNo, setPageNo] = useState(1); // 현재 활성화 된 페이지 번호
+  const [listSize, setListSize] = useState(10); // 한 페이지에 나타낼 document 개수
+
+  /* 데이터 정제하기 */
+  const dataCleansing = (rawData) => {
+    let _archiveDataList = [];
+    let _rawArchiveDataList = rawData.docs;
+    let _dcCount = rawData.dcCount;
+
+    _rawArchiveDataList.forEach((item, index) => {
+      const obj = {
+        dc_title_or: item.dc_title_or,
+        dc_title_kr: item.dc_title_kr,
+        dc_keyword: item.dc_keyword,
+        dc_dt_collect: item.dc_dt_collect,
+        item_id: Number(item.item_id),
+        stat: item.stat,
+      };
+
+      _archiveDataList.push(obj);
+    });
+    setDcCount(_dcCount);
+    setArchiveDataList(_archiveDataList);
+  };
+
+  /* 데이터 불러오기 */
+  const dataFetch = () => {
+    CrawlDataListFetchApi(6, listSize, pageNo).then((res) => {
+      dataCleansing(res.data);
+      console.log(res.data)
+    });
+  };
+
+  useEffect(() => {
+    dataFetch();
+  }, [pageNo]);
+
   return (
     <>
-      <ArchiveDataList />
+      <ArchiveDataList archiveDataList={archiveDataList} />
     </>
   );
 }
