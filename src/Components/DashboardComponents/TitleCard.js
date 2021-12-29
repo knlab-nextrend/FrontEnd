2f2f9c037a0ac8804d6a7db17287907be9c8bbc0
DESKTOP-N9NO675\hayoungzzzzz <hayoung474@gmail.com>
2021-12-29 21:54:18 +0900

    국가별 스크리닝 및 정제 등록 큐레이션 문서 수 카드 컴포넌트 껍데기 제작

--- src/Components/DashboardComponents/TitleCard.js
+++ src/Components/DashboardComponents/TitleCard.js
@@ -3,21 +3,32 @@ import styled from "styled-components";
 function TitleCard() {
   return (
     <CardWrapper>
-      <div className="title-main"></div>
-      <div className="title-sub"></div>
+      <div className="title">
+        <div className="title-main">국가별 문서 현황</div>
+        <div className="title-sub">
+          국가를 클릭하면 해당 국가에 대한 문서 작업 현황을 확인할 수 있습니다.
+        </div>
+      </div>
     </CardWrapper>
   );
 }
 
 const CardWrapper = styled.div`
-  border-radius: 12px;
-  margin: 1rem;
-  border:1px solid #E6E9ED;
-  .title-main{
-    border-bottom:1px solid #E6E9ED;
+  border-radius: 4px;
+  box-shadow: 0 0 0.875rem 0 rgba(33, 37, 41, 0.05);
+  background-color:white;
+  .title{
+    border-bottom: 1px solid #e6e9ed;
+    padding:1rem;
   }
-  .title-sub{
-
+  .title-main {
+    font-size: 18px;
+    color: rgb(59, 59, 59);
+    padding-bottom:0.25rem;
+  }
+  .title-sub {
+    font-size: 12px;
+    color: #939ba2;
   }
 `;
 export default TitleCard;
--- src/Pages/Manager/Dashboard/Dashboard.js
+++ src/Pages/Manager/Dashboard/Dashboard.js
@@ -1,10 +1,28 @@
 import React from "react";
-import TitleCard from "../../../Components/DashboardComponents/TitleCard"
-function Dashboard (){
-    return (
-        <>
+import TitleCard from "../../../Components/DashboardComponents/TitleCard";
+import DocumentStatCard from "../../../Components/DashboardComponents/DocumentStatCard";
+import FormHeader from "../../../Components/FormHeader";
+import styled from "styled-components";
+
+function Dashboard() {
+  return (
+    <>
+      <FormHeader type={"view"} title={"대시보드"} />
+      <Wrapper>
+        <CountryContentWrapper>
           <TitleCard></TitleCard>
-        </>
-      );
+          <DocumentStatCard></DocumentStatCard>
+        </CountryContentWrapper>
+      </Wrapper>
+    </>
+  );
 }
+
+const Wrapper = styled.div`
+  background-color: #eee;
+  padding: 1rem;
+`;
+const CountryContentWrapper = styled.div`
+  display: flex;
+`;
 export default Dashboard;
