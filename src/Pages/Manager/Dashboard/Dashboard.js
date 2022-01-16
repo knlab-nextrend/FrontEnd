22e46c3d428735d2ad02a2d13c6364f7d1387f3d
Baekwangho <qorhkdgh00@gmail.com>
2022-01-16 23:53:58 +0900

    temp

--- src/Pages/Manager/Dashboard/Dashboard.js
+++ src/Pages/Manager/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+import { Component } from "react";
 import TitleCard from "../../../Components/DashboardComponents/TitleCard";
 import DocumentStatCard from "../../../Components/DashboardComponents/DocumentStatCard";
 import FormHeader from "../../../Components/FormHeader";
@@ -12,7 +13,52 @@ import styled from "styled-components";
 
 import { ResponsivePie } from "@nivo/pie"; // 원형차트 임시...
 import { ResponsiveGeoMap } from "@nivo/geo"; // 세계지도!
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 
+
+function Map(props) {
+  useLayoutEffect(() => {
+    
+    let root = am5.Root.new("chartdiv");
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    let chart = root.container.children.push( 
+      am5map.MapChart.new(root, {
+        //panX: "rotateX",
+        projection: am5map.geoEquirectangular()
+      })
+    );
+    let polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_worldLow,
+        exclude: ["AQ"]
+      })
+    );
+
+    polygonSeries.mapPolygons.template.setAll({
+      tooltipText: "{name}",
+      interactive: true
+    });
+    polygonSeries.mapPolygons.template.states.create("hover", {
+      fill: am5.color(0x677935)
+    });
+    chart.set("zoomControl", am5map.ZoomControl.new(root, {}));
+    return () => {
+      root.dispose();
+    };
+  }, []);
+
+  return (
+    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+  );
+}
 function Dashboard({ data }) {
   return (
     <>
@@ -23,7 +69,9 @@ function Dashboard({ data }) {
           <WorkStatCard/>
 
         <CountryContentWrapper>
-          <TitleCard></TitleCard>
+          <TitleCard>
+            <Map></Map>
+          </TitleCard>
           <div>
             <ChartCard>
               <ResponsivePie
