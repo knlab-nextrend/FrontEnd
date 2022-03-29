cd231316a8a11db5eb2ecec7e44f10bcd398b81d
hayoungzzzzz <hayoung474@gmail.com>
2022-03-29 14:06:35 +0900

    x축,y축이 설정되지 않은 사용자는 로그인하지 못하게끔 수정

--- src/Pages/User/UserOnlyDataLookUpPage/UserOnlyDataLookUpPageContainer.js
+++ src/Pages/User/UserOnlyDataLookUpPage/UserOnlyDataLookUpPageContainer.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from "react";
 import UserOnlyDataLookUpPage from "./UserOnlyDataLookUpPage";
 import { userAxisMenuFetchApi } from "../../../Utils/api";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setLogout } from "../../../Modules/login";
 
 function UserOnlyDataLookUpPageContainer() {
+  const dispatch = useDispatch();
   const userInfo = useSelector((state) => state.user.user);
   const [axisMenu, setAxisMenu] = useState({ X: [], Y: [] });
 
   const menuDataFetch = (uid) => {
     userAxisMenuFetchApi(uid).then((res) => {
-        console.log(res.data)
-      const _axisMenu = { X: res.data.axis_x, Y: res.data.axis_y };
-      setAxisMenu(_axisMenu);
+      console.log(res.data);
+      if (res.data.axis_x.length === 0 || res.data.axis_y.lengtth === 0) {
+        alert("x축, y축 메뉴가 모두 설정되지 않은 사용자입니다.");
+        dispatch(setLogout("NORMAL_LOGOUT"));
+      } else {
+        const _axisMenu = { X: res.data.axis_x, Y: res.data.axis_y };
+        setAxisMenu(_axisMenu);
+      }
     });
   };
   useEffect(() => {
-    console.log(userInfo);
     menuDataFetch(userInfo.id);
   }, [userInfo]);
   return (
     <>
-      <UserOnlyDataLookUpPage axisMenu={axisMenu}/>
+      <UserOnlyDataLookUpPage axisMenu={axisMenu} />
     </>
   );
 }
